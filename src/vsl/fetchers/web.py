9fd1d2451b9cbf70c8e9fafd3a08729db0067f2f
איתי בן יעקב <80927047+begnac@users.noreply.github.com>
2023-10-16 13:10:27 +0200

    increase WebSearch score.

--- src/vsl/fetchers/web.py
+++ src/vsl/fetchers/web.py
@@ -124,7 +124,7 @@ class FetcherWebSearch(base.FetcherLeaf):
 
     def do_request(self, request):
         self.reply.remove_all()
-        self.append_item(items.ItemUri(name=self.name, detail=self.url.replace('%s', request), icon=self.icon), score=0.5)
+        self.append_item(items.ItemUri(name=self.name, detail=self.url.replace('%s', request), icon=self.icon), score=0.7)
 
 
 class FetcherUrl(base.FetcherLeaf):
